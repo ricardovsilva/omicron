540e30c7cc2c314ac8cf625c5d1cd59d2a2da7e0
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2017-08-15 23:32:13 -0300

    Creates basic project structure

---
+++ omicron.domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace omicron.domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ omicron.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace omicron.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
